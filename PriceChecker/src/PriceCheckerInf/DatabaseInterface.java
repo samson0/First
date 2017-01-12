d08460b146ade2771df0080960b437191cd61356
samson0 <sansen777@hotmail.com>
2017-01-12 11:02:36 +0800

    delete

--- PriceChecker/src/PriceCheckerInf/DatabaseInterface.java
+++
@@ -1,7 +0,0 @@
-package PriceCheckerInf;
-
-public interface DatabaseInterface {
-	public boolean connect_DB();
-	public void disconnect_DB();
-	public boolean queryProductInfo(String strBarcode, String[] rPrice, String[] rProductName, String[] rProductImage);
-}
